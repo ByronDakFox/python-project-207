2dab2a0b9d174fa9ef68c6a84c6e8bc8a4397e7f
Byron Ramirez <tauren62@gmail.com>
2026-05-24 22:11:35 -0500

    edit project
    modificación del proyecto

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv(
+    'SECRET_KEY',
+    'dev-secret-key'
+)
 
 @app.route('/')
 def index():
