b616f62d0dc89985dc60fa91b034ed214f2829bb
Byron Ramirez <tauren62@gmail.com>
2026-06-01 20:23:31 -0500

    update 3.5
    actualizacion de app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -21,11 +21,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+#conn = psycopg2.connect(DATABASE_URL)
 
 
-#def get_connection():
-#   return psycopg2.connect(DATABASE_URL)
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
 
 @app.route('/')
