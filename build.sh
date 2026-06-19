9be2d1db10176f051ba37f30a85ad7e66a7777c2
Byron Ramirez <tauren62@gmail.com>
2026-06-18 20:46:39 -0500

    update part6
    cambio en .env

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 source $HOME/.local/bin/env
 
-make install && psql -a -d $DATABASE_URL -f database.sql
+DATABASE_URL=postgresql://dbanalyzer_zjfi_user:3FHmEgMyH82Ly8Xqu5TSWQ8kk4xgLGm4@dpg-d8jm6ul8nd3s73fq1gbg-a.virginia-postgres.render.com/dbanalyzer_zjfi
+
+make install && psql $DATABASE_URL -f database.sql
